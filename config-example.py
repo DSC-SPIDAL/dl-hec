ebe3ebd2ea6b72358465a38e6490a7d7a2c295e9
Gregor von Laszewski <laszewski@gmail.com>
2022-12-27 13:22:12 -0500

    demonstrate config

---
+++ config-example.py
@@ -0,0 +1,17 @@
+from cloudmesh.common.FlatDict import FlatDict, read_config_parameters, flatten
+from cloudmesh.common.util import readfile
+
+def read_config(filename):
+    config_dict = read_config_parameters(filename)
+    config = FlatDict(config_dict, sep=".")
+    del config["sep"]
+    return config
+
+config = read_config("./config.yaml")
+print (config)
+
+print(config["covid.ReadMay2022Covid"])
+
+ReadMay2022Covid = config["covid.ReadMay2022Covid"]
+
+print (ReadMay2022Covid)
