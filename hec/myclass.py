af0cf14f645048ef5653c06f9e87e93d5f371b89
Gregor von Laszewski <laszewski@gmail.com>
2023-01-03 09:31:15 -0500

    add a class example

---
+++ hec/myclass.py
@@ -0,0 +1,11 @@
+class myclass:
+
+    def __init__(self):
+        self.a = "a"
+        self.b = "b"
+        self.c = "c"
+
+    def print_values(self):
+        print(self.a)
+        print(self.b)
+        print(self.c)
