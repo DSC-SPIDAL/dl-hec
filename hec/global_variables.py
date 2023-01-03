7aabcbc5c1969a55b4e4f27fc569e2d1604c7d88
Gregor von Laszewski <laszewski@gmail.com>
2023-01-03 09:17:20 -0500

    improve ipynb tutorial

---
+++ hec/global_variables.py
@@ -0,0 +1,7 @@
+# global variables have to be initialized first
+# they can then be imported from this file as
+# from hec.global_variables import my_global_a
+# to decaler the in methods as global my_global_a
+my_global_a = None
+my_global_b = "b"
+my_global_c = "c"
